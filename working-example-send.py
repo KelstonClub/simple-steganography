2fe885797042eb548c3cbbc68712151690fe5ee0
Tim Golden <mail@timgolden.me.uk>
2020-01-26 17:59:07 +0000

    Tidy up

--- working-example-send.py
+++ working-example-send.py
@@ -6,38 +6,8 @@ from PIL import Image
 # pixels is equals to the "pixel area" of
 # the image
 #
-dragon = Image.open("dragon.jpg")
-data = dragon.getdata()
-
-## b_with_0_last_bit = b & 0xfe
-## b_with_1_last_bit = b | 1
-
-#
-# Get the colours of the top-left pixel
-#
-r, g, b = data.getpixel((0, 0))
-#
-# Set the last bit of b to zero
-#
-b &= 0xfe
-data.putpixel((0, 0), (r, g, b))
-
-#
-# Get the colours of the next pixel
-#
-r, g, b = data.getpixel((1, 0))
-#
-# Set the last bit of b to 1
-#
-b |= 1
-data.putpixel((1, 0), (r, g, b))
-
-#
-# Put the updated data back into the image
-# and save it under a different name
-#
-dragon.putdata(data)
-dragon.save("dragon2.jpg")
+image = Image.open("image.jpg")
+data = image.getdata()
 
 def message_as_bits(message):
     #
@@ -66,7 +36,7 @@ def blue_final_bit(rgb, bit):
 
 def encoded(image, message):
     data = image.getdata()
-    message_bits = message_as_bits(image, message)
+    message_bits = message_as_bits(message)
     message_len = len(message)
     if message_len > 255:
         raise RuntimeError("Message must be no longer than 255 letters")
@@ -83,7 +53,7 @@ def encoded(image, message):
     # NB doing it this way avoids having to map between
     # groups of 8 bits and coordinates in an image.
     #
-    for row, bits in zip(range(data.height, message)):
+    for row, bits in enumerate(message_bits):
         for column, bit in enumerate(bits):
             rgb = data.getpixel((column, row))
             data.putpixel((column, row), blue_final_bit(rgb, bit))
@@ -94,6 +64,6 @@ def encoded(image, message):
 if __name__ == '__main__':
     image_filepath = input("Enter an image filename: ")
     message = input("Enter a message: ")
-    dragon = Image.open(image_filepath)
-    encoded_dragon = encoded(dragon, message)
-    encoded_dragon.save("encoded_dragon.jpg")
+    image = Image.open(image_filepath)
+    encoded_image = encoded(image, message)
+    encoded_image.save("encoded.jpg")
